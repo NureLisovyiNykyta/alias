ffc3b84c90e6a32bf4bf86cb96d0812385c25c83
Oleksandr Kozhanov <oleksandr.kozhanov@nure.ua>
2026-04-20 14:11:35 +0300

    added button and fonts configs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
+import { Button } from "./components/Button.jsx";
+
 function App() {
   return (
-    <div>
-      hello world
+    <div className='min-h-screen w-full p-4'>
+      <div className='flex w-full justify-center items-center h-full gap-3 flex-col'>
+        <div className='flex gap-3'>
+          <Button disabled={false} variant='primary'>Primary</Button>
+          <Button disabled={true} variant='primary'>Primary</Button>
+        </div>
+
+        <div className='flex gap-3'>
+          <Button disabled={false} variant='secondary'>Secondary</Button>
+          <Button disabled={true} variant='secondary'>Secondary</Button>
+        </div>
+
+        <div className='flex gap-3'>
+          <Button disabled={false} variant='tertiary'>Tertiary</Button>
+          <Button disabled={true} variant='tertiary'>Tertiary</Button>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const baseStyles = "inline-flex items-center justify-center gap-2 font-noto text-btn font-semibold rounded-[12px] cursor-pointer disabled:cursor-not-allowed disabled:pointer-events-none transition-all";
+const variants = {
+  primary: `
+    bg-brand-500 text-text
+    hover:bg-brand-700 hover:text-white
+    active:bg-brand-900 active:text-white
+    disabled:bg-surface disabled:text-text-label
+    h-[48px]
+    w-[144px]
+  `,
+  secondary: `
+    bg-brand-100 text-text
+    hover:bg-brand-300
+    active:bg-brand-500
+    disabled:bg-neutral-100 disabled:text-text-label  
+    h-[48px]    
+    w-[144px]
+  `,
+  tertiary: `
+    bg-white border-2 border-surface shadow-buttons text-text
+    hover:bg-white hover:border-text-label
+    active:bg-white active:border-brand-500
+    disabled:border-surface disabled:text-text-label
+    h-[48px]
+    w-[144px]
+  `,
+};
+
+const sizes = {
+  default: "px-8",
+  icon: "p-2.5 aspect-square"
+};
+
+export const Button = ({
+                         variant = 'primary',
+                         size = 'default',
+                         disabled = false,
+                         className = '',
+                         children,
+                         ...props
+                       }) => {
+  return (
+    <button
+      disabled={disabled}
+      className={`${baseStyles} ${variants[variant]} ${sizes[size]} ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
