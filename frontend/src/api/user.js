1602aad45c9f2f245a9dbeca0e56f2d234589ea1
Oleksandr Kozhanov <oleksandr.kozhanov@nure.ua>
2026-05-07 16:12:38 +0300

    implemented profile endpoints UI integration

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -1,5 +1,5 @@
 import { api } from "./axios";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
 export const getUserByUsername = async (username) => {
   const response = await api.get(`/users/${username}`);
@@ -11,6 +11,36 @@ export const useUserByUsernameQuery = (username, options) => {
     queryKey: ['user', username],
     queryFn: () => getUserByUsername(username),
     enabled: !!username,
+    ...options,
+  });
+};
+
+export const useUpdateMeMutation = (options) => {
+  return useMutation({
+    mutationFn: async (data) => {
+      const response = await api.patch('/users/me', data);
+      return response.data;
+    },
+    ...options,
+  });
+};
+
+export const useChangePasswordMutation = (options) => {
+  return useMutation({
+    mutationFn: async (data) => {
+      const response = await api.post('/users/me/password', data);
+      return response.data;
+    },
+    ...options,
+  });
+};
+
+export const useDeleteMeMutation = (options) => {
+  return useMutation({
+    mutationFn: async () => {
+      const response = await api.delete('/users/me');
+      return response.data;
+    },
     ...options,
   });
 };
